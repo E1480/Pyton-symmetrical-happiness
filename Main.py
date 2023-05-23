2ec451364c797f5307d3ce44c0bd38639d1a5767
E1480 <that1480devguy@gmail.com>
2023-05-23 22:18:30 +0800

    commit

--- Main.py
+++ Main.py
@@ -1,21 +1,7 @@
-import cv2
 
-class App:
-    def __init__(self, camera: int = 0) -> None:
-        self.camera = camera
-        self.vid = cv2.VideoCapture(self.camera)
-    
-    def Loop(self):
-        while True:
-            ret, frame = self.vid.read()
-            
-            cv2.imshow('Window', frame)
-            
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
+class Caesar_Decipher:
+    def __init__(self) -> None:
+        pass
         
-        self.vid.release()
-        cv2.destroyAllWindows()
-
 if __name__ == "__main__":
-    App().Loop()
+    Caesar_Decipher()
